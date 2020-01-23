21b8fbfd94c0c011efb141aa3c7e97bf0f2feb65
jevdee <jev.deee@gmail.com>
2020-01-23 17:53:42 +0900

    Solve CommonChild

---
+++ HackerRank/src/main/java/Array2DDS.java
@@ -0,0 +1,5 @@
+public class Array2DDS {
+    public static void main(String[] args) {
+
+    }
+}
--- HackerRank/src/main/java/CommonChild.java
+++ HackerRank/src/main/java/CommonChild.java
@@ -1,5 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CommonChild {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+//        SHINCHAN
+//        NOHARAAA
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+        int n = a.length();
+        int[][] arr = new int[n+1][n+1];
+
+        for(int i=1; i<n+1; i++) {
+            for (int j = 1; j < n+1; j++) {
+                if(a.charAt(i-1) == b.charAt(j-1)) {
+                    arr[i][j] = arr[i-1][j-1] + 1;
+                } else {
+                    arr[i][j] = Math.max(arr[i-1][j], arr[i][j-1]);
+                }
+            }
+        }
+
+        System.out.println(arr[n][n]);
+
 
     }
 
