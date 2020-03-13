82e124d4b470d8fd7c5c33178bc50eca677d9d3e
jevdee <jev.deee@gmail.com>
2020-03-13 14:43:42 +0900

    Candies - finish

--- HackerRank/src/main/java/Candies.java
+++ HackerRank/src/main/java/Candies.java
@@ -4,80 +4,39 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Candies {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int length = scan.nextInt();
 
-        if(n < 2) {
-            System.out.println(1);
-            return;
+        int[] children = new int[length];
+        int[] candies = new int[length];
+
+        // seed
+        children[0] = scan.nextInt();
+        candies[0] = 1;
+
+        // search forward sequences
+        for (int i = 1; i < length; i++){
+            children[i] = scan.nextInt();
+            candies[i] = 1;
+            if (children[i] > children[i - 1])
+                candies[i] = candies[i - 1] + 1;
         }
 
-        int[] arr = new int[n];
-        int[] resArr = new int[n];
-        int i=0;
-        int j=0;
-        for (int k = 0; k < n; k++) {
-            arr[k] = Integer.parseInt(br.readLine());
+        // search reverse sequences
+        for (int i = length - 1; i > 0; i--){
+            if (children[i] < children[i - 1])
+                candies[i - 1] = Math.max(candies[i - 1], candies[i] + 1);
         }
 
-        while(i < n) {
-            int start = i;
-            Set<Integer> set = new HashSet<>();
-            List<Integer> list = new ArrayList<>();
-            while(true) {
-                set.add(arr[i]);
-                list.add(arr[i]);
-                if (arr[i] < arr[i + 1]) {
-                    i++;
-                    break;
-                }
-                i++;
-            }
-
-            int maxNum = set.size();
-            if(j != 0 && maxNum < resArr[j-1]) {
-                maxNum = resArr[j-1] + 1;
-            }
-
-            if(set.size() > 1 && list.size() == set.size()) {   //여러종류의 숫자가 중복없음  4 3 2
-                for (int k=0;  k<list.size(); k++) {
-                    resArr[j] = maxNum;
-                    maxNum--;
-                    j++;
-                }
-            } else if(set.size() == 1 && list.size() > 1) {    //한종류의 숫자가 여러개 중복 2 2 2 2
-                resArr[j] = maxNum;
-                j++;
-                for (int k=1;  k<list.size(); k++) {
-                    resArr[j] = 1;
-                    j++;
-                }
-            } else if(set.size() > 1 && list.size() > set.size()) { //여러종류의 숫자가 여러개이면서 중복숫자 포함 3 3 2, 3 2 2, 3 2 2 1 1
-                resArr[j] = maxNum;
-                j++;
-                start++;
-
-                for (int k = 1; k < list.size(); k++,start++) {
-                    if(arr[start-1] == arr[start]) {
-                        resArr[j] = maxNum;
-                    } else {
-                        maxNum--;
-                        resArr[j] = maxNum;
-                    }
-                }
-            } else {        //한종류의 숫자가 1개
-                resArr[j] = maxNum;
-                j++;
-            }
+        long sum = 0;
+        for (int i= 0; i < candies.length; i++){
+            sum += candies[i];
+            System.out.println(candies[i]);
         }
 
-
-
-
-        System.out.println(Arrays.stream(resArr).sum());
+        System.out.println(sum);
 
     }
-
 
 }
